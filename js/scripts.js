b1e953186fede45f3a89bc0aa7885f0927c4f7af
David Boedigheimer <stealth259@gmail.com>
2019-10-24 14:52:40 -0700

    Add puzzle function with for and forEach loops in it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 // Back end
-
+var puzzle = function(quote) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var sentence = quote.split("");
+  for (var i = 0; i < sentence.length; i++) {
+    vowels.forEach(function(vowel) {
+      if (sentence[i] === vowel) {
+        sentence[i] = "-"
+      }
+    });
+  }
+  return sentence.join("")
+}
 // Front end
 $(document).ready(function() {
   $("#form-one").submit(function(e) {
+    e.preventDefault();
     var quote = $("input#quote").val();
-    
+    var newSentence = puzzle(quote);
 
-    e.preventDefualt();
+    $("#puzzle").append(newSentence);
   });
 });
